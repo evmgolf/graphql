1467eada0d4fa510b34082dba46f34752c0078a1
evmgolf <evmgolf@github.com>
2022-05-31 08:36:23 +0100

    Created a minimal GraphQL implementation for decoding address[] results

--- src/GraphQL.sol
+++ src/GraphQL.sol
@@ -3,26 +3,18 @@ pragma solidity 0.8.13;
 
 import {Script} from "forge-std/Script.sol";
 import {Decimal} from "codec/Decimal.sol";
+import {Bash} from "bash/Bash.sol";
 
 contract GraphQL is Script {
   using Decimal for uint;
 
-  function shell(bytes memory script, bytes memory filename) public returns (bytes memory) {
-    string[] memory args = new string[](3);
-    args[0] = "bash";
-    args[1] = "-c";
-    if (filename.length > 0) {
-      script = bytes.concat(script, ">", filename);
-    }
-    args[2] = string(script);
-    return vm.ffi(args);
-  }
-
   function queryManyAddresses(bytes memory url, bytes memory query, bytes memory entityName, bytes memory fieldName) public returns (address[] memory results) {
+    Bash bash = new Bash();
+
     bytes memory filename = "result.json";
     bytes memory resultSelector = bytes.concat(".data.", entityName);
     
-    shell(
+    bash.run(
       bytes.concat(
         "curl ",
         url,
@@ -33,19 +25,39 @@ contract GraphQL is Script {
       filename
     );
 
-    uint resultSize = abi.decode(shell(bytes.concat("printf \"%.64x\" $(jq -r '", resultSelector, "|length' ", filename, ")"), ""), (uint));
+    uint resultSize = abi.decode(
+      bash.run(
+        bytes.concat(
+          "printf \"%.64x\" $(jq -r '",
+          resultSelector,
+          "|length' ",
+          filename,
+          ")"
+        ),
+        ""
+      ),
+      (uint)
+    );
+
     results = new address[](resultSize);
     for (uint i=0; i<resultSize;i++) {
-      results[i] = abi.decode(shell(bytes.concat("cast --concat-hex 0x000000000000000000000000 $(jq -r '", resultSelector, "[", i.decimal(), "].", fieldName, "' ", filename, ")"), ""), (address));
+      results[i] = abi.decode(
+        bash.run(
+          bytes.concat(
+            "cast --concat-hex 0x000000000000000000000000 $(jq -r '",
+            resultSelector,
+            "[",
+            i.decimal(),
+            "].",
+            fieldName,
+            "' ",
+            filename,
+            ")"
+          ),
+          ""
+        ),
+        (address)
+      );
     }
-  }
-
-  function run() external {
-    queryManyAddresses(
-      "http://127.0.0.1:8000/subgraphs/name/evmgolf/evmgolf-subgraph",
-      "query {challengeEntities {id}}",
-      "challengeEntities",
-      "id"
-    );
   }
 }
--- src/test/GraphQL.t.sol
+++ src/test/GraphQL.t.sol
@@ -2,3 +2,25 @@
 pragma solidity 0.8.13;
 
 import "forge-std/Test.sol";
+import {GraphQL} from "../GraphQL.sol";
+
+contract GraphQLTest is Test {
+  GraphQL graphql;
+
+  function setUp() public {
+    graphql = new GraphQL();
+  }
+
+  function testQueryManyAddresses() public {
+    address[] memory results = graphql.queryManyAddresses(
+      "https://api.thegraph.com/subgraphs/name/evmgolf/evmgolf-mumbai",
+      "query {challengeEntities {id}}",
+      "challengeEntities",
+      "id"
+    );
+    assertGt(results.length, 0);
+    for (uint i=0;i<results.length;i++) {
+      assertTrue(results[i] != address(0));
+    }
+  }
+}
