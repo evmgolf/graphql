1df822999d0ce26ac16e58117d00581626305089
evmgolf <evmgolf@github.com>
2022-06-09 13:51:42 +0100

    Reworked GraphQL to follow a query constructor + executor model.

--- src/GraphQL.sol
+++ src/GraphQL.sol
@@ -5,59 +5,74 @@ import {Script} from "forge-std/Script.sol";
 import {Decimal} from "codec/Decimal.sol";
 import {Bash} from "bash/Bash.sol";
 
-contract GraphQL is Script {
+library GraphQL {
+  function args(bytes[] memory keys, bytes[] memory values) internal pure returns (bytes memory text) {
+    text = "(";
+    if (keys.length > 0) {
+      text = bytes.concat(
+        text,
+        keys[0],
+        ": ",
+        values[0]
+      );
+    }
+    for (uint i=1;i<keys.length;i++) {
+      text = bytes.concat(
+        text,
+        ", ",
+        keys[i],
+        ": ",
+        values[i]
+      );
+    }
+    text = bytes.concat(text, ")");
+  }
+
+  function selectionSet(bytes[] memory selections) internal pure returns (bytes memory text) {
+    text = "{";
+    for (uint i=0;i<selections.length;i++) {
+      text = bytes.concat(
+        text,
+        " ",
+        selections[i]
+      );
+    }
+    text = bytes.concat(text, " }");
+  }
+
+  function field(bytes memory name, bytes memory _args, bytes memory _selectionSet) internal pure returns (bytes memory) {
+    return bytes.concat(
+      name,
+      _args,
+      " ",
+      _selectionSet
+    );
+  }
+
+  function query(bytes memory _selectionSet) internal pure returns (bytes memory) {
+    return bytes.concat("query ", _selectionSet);
+  }
+}
+
+contract GraphQLExecutor is Script {
   using Decimal for uint;
 
-  function queryManyAddresses(bytes memory url, bytes memory query, bytes memory entityName, bytes memory fieldName) public returns (address[] memory results) {
+  function query(
+    bytes memory url,
+    bytes memory queryText
+  ) public returns (bytes memory filename) {
     Bash bash = new Bash();
+    filename = bash.run("mktemp|cast --from-utf8");
 
-    bytes memory filename = "result.json";
-    bytes memory resultSelector = bytes.concat(".data.", entityName);
-    
     bash.run(
       bytes.concat(
         "curl ",
         url,
         " -H 'Content-Type: application/json' -X POST -d '{\"query\":\"",
-        query,
+        queryText,
         "\"}'"
       ),
       filename
     );
-
-    uint resultSize = abi.decode(
-      bash.run(
-        bytes.concat(
-          "printf \"%.64x\" $(jq -r '",
-          resultSelector,
-          "|length' ",
-          filename,
-          ")"
-        ),
-        ""
-      ),
-      (uint)
-    );
-
-    results = new address[](resultSize);
-    for (uint i=0; i<resultSize;i++) {
-      results[i] = abi.decode(
-        bash.run(
-          bytes.concat(
-            "cast --concat-hex 0x000000000000000000000000 $(jq -r '",
-            resultSelector,
-            "[",
-            i.decimal(),
-            "].",
-            fieldName,
-            "' ",
-            filename,
-            ")"
-          ),
-          ""
-        ),
-        (address)
-      );
-    }
   }
 }
--- src/test/GraphQL.t.sol
+++ src/test/GraphQL.t.sol
@@ -2,25 +2,71 @@
 pragma solidity 0.8.13;
 
 import "forge-std/Test.sol";
-import {GraphQL} from "../GraphQL.sol";
+import {JSONQuery, JSONExecutor} from "jsonquery/JSONQuery.sol";
+import {GraphQL, GraphQLExecutor} from "../GraphQL.sol";
 
 contract GraphQLTest is Test {
-  GraphQL graphql;
+  using JSONQuery for bytes;
+
+  JSONExecutor json;
+  GraphQLExecutor graphql;
 
   function setUp() public {
-    graphql = new GraphQL();
+    json = new JSONExecutor();
+    graphql = new GraphQLExecutor();
   }
 
-  function testQueryManyAddresses() public {
-    address[] memory results = graphql.queryManyAddresses(
+  function testBuildQuery() public {
+    bytes[] memory argKeys = new bytes[](1);
+    bytes[] memory argValues = new bytes[](1);
+    argKeys[0] = "";
+    argValues[0] = "";
+    bytes[] memory selectionSet = new bytes[](1);
+    selectionSet[0] = "id";
+
+    GraphQL.query(
+      GraphQL.field(
+        "challengEntities",
+        GraphQL.args(argKeys, argValues),
+        GraphQL.selectionSet(selectionSet)
+      )
+    );
+  }
+
+  function testExecuteArray() public {
+    bytes memory query;
+    {
+      // bytes[] memory argKeys = new bytes[](1);
+      // bytes[] memory argValues = new bytes[](1);
+      // argKeys[0] = "";
+      // argValues[0] = "";
+      bytes[] memory selectionSet = new bytes[](1);
+      selectionSet[0] = "id";
+
+      bytes[] memory topSelectionSet = new bytes[](1);
+      topSelectionSet[0] = GraphQL.field(
+        "challengeEntities",
+        "",
+        // GraphQL.args(argKeys, argValues),
+        GraphQL.selectionSet(selectionSet)
+      );
+
+      query = GraphQL.selectionSet(topSelectionSet);
+    }
+
+    bytes memory filename = graphql.query(
       "https://api.thegraph.com/subgraphs/name/evmgolf/evmgolf-mumbai",
-      "query {challengeEntities {id}}",
-      "challengeEntities",
-      "id"
+      query
+    );
+
+    bytes[] memory results = json.readArray(
+      filename,
+      bytes("").query("data").query("challengeEntities"),
+      bytes("").query("id")
     );
     assertGt(results.length, 0);
     for (uint i=0;i<results.length;i++) {
-      assertTrue(results[i] != address(0));
+      assertGt(results[i].length, 0);
     }
   }
 }
